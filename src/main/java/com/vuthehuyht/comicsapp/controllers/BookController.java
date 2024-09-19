f78a300c0861619ae3024ef814f400d613c6d0a1
Vu The Huy <vuthehuy.t61@gmail.com>
2024-09-19 20:01:42 +0700

    fix: remove slash

--- src/main/java/com/vuthehuyht/comicsapp/controllers/BookController.java
+++ src/main/java/com/vuthehuyht/comicsapp/controllers/BookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/")
+@RequestMapping(path = "/api/v1")
 @RequiredArgsConstructor
 public class BookController {
     final private BookService bookService;
